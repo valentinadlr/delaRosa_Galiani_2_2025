7f26688ec07c783fd800460a9bbde966c8f8ff0b
fioregaliani <fiorella.galiani@ingenieria.uner.edu.ar>
2025-11-14 15:52:40 -0300

    se sube intento de uart y modificacion de función teclas

--- firmware/proyecto/main/proyecto.c
+++ firmware/proyecto/main/proyecto.c
@@ -37,26 +37,34 @@
 #include <stdio.h>
 #include <stdint.h>
 #include "gpio_mcu.h"
+#include "uart_mcu.h"
+#include "esp_log.h"
 
-#define RELE_PIN GPIO_19 // pin para el relé
-#define NEOPIXEL_PIN GPIO_18 //pin neopixel
+#define RELE_PIN GPIO_19     // pin para el relé
+#define NEOPIXEL_PIN GPIO_18 // pin neopixel
 #define NEOPIXEL_LEN 12
 #define VASO_UMBRAL_CM 5
-#define SEGUNDOS_ENTRE_DESCARGAS 20 // 30 minutos
-#define TIEMPO_REFRESCO_CONTADOR 1 // 1 segundo
+#define SEGUNDOS_ENTRE_DESCARGAS 1800 // 30 minutos
+#define TIEMPO_REFRESCO_CONTADOR 1    // 1 segundo
 
 static neopixel_color_t colores[NEOPIXEL_LEN];
 static uint32_t contador_segundos = 0;
 
+int putchar(int c) {
+    char ch = (char)c;
+    UartSendByte(UART_PC, &ch);
+    return c;
+}
+
 void BombaOn(void)
 {
-    GPIOState(RELE_PIN, false);   // activa el relé
+    GPIOState(RELE_PIN, false); // activa el relé
     printf("💡 Bomba encendida\n");
 }
 
 void BombaOff(void)
 {
-    GPIOState(RELE_PIN, true); //apaga el relé
+    GPIOState(RELE_PIN, true); // apaga el relé
     printf("💡 Bomba apagada\n");
 }
 
@@ -111,44 +119,66 @@ void TareaPrincipal(void *pvParameters)
  */
 void TareaTeclas(void *pvParameters)
 {
+    int8_t estado;
+
     while (1)
     {
-        int8_t estado = SwitchesRead();
+        estado = SwitchesRead();
 
-        if (estado & SWITCH_1)
+        switch (estado)
         {
-            printf("🔘 Tecla 1 presionada — dispensar inmediato\n");
-            contador_segundos = SEGUNDOS_ENTRE_DESCARGAS; // fuerza evento
-        }
+        case SWITCH_1:
+            // podemos agregar mensaje por la uart
+            contador_segundos = SEGUNDOS_ENTRE_DESCARGAS;
+            break;
 
-        else if (estado & SWITCH_2)
-        {
-            printf("🔘 Tecla 2 presionada — reiniciar contador\n");
+        case SWITCH_2:
+            // podemos agregar mensaje por la uart
             contador_segundos = 0;
+            break;
+
+        default:
+            break;
         }
 
-        DelayMs(200); 
+        DelayMs(200);
     }
+}
+
+
+int my_vprintf(const char *fmt, va_list args)
+{
+    char buffer[256];
+    int len = vsnprintf(buffer, sizeof(buffer), fmt, args);
+
+    uart_write_bytes(UART_NUM_0, buffer, len); // UART por USB
+    return len;
 }
 
 void app_main(void)
 {
+    serial_config_t uart_cfg = {
+        .port = UART_PC,
+        .baud_rate = 115200,
+        .func_p = UART_NO_INT,
+        .param_p = NULL};
+
+    UartInit(&uart_cfg);
+
+    esp_log_set_vprintf(my_vprintf); //saca por la uart todos los print 
+
     printf("🚰 Iniciando Mini Dispenser Inteligente...\n");
 
-    // Inicialización de periféricos
     HcSr04Init(GPIO_3, GPIO_2); // echo, trigger
     SwitchesInit();
 
-    // Inicialización del relé
     GPIOInit(RELE_PIN, GPIO_OUTPUT);
     GPIOOn(RELE_PIN);
 
-    // Inicialización del neo pixel
     NeoPixelInit(NEOPIXEL_PIN, NEOPIXEL_LEN, colores);
     NeoPixelBrightness(80);
     NeoPixelAllColor(NEOPIXEL_COLOR_BLUE);
 
-    // Crear tareas
     xTaskCreate(TareaPrincipal, "TareaPrincipal", 2048, NULL, 1, NULL);
     xTaskCreate(TareaTeclas, "TareaTeclas", 2048, NULL, 1, NULL);
 }
